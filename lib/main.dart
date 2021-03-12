f11040e371ece2cefb8a577300654889e7bf3436
kys3923 <kys3923@gmail.com>
2021-03-12 10:41:09 -0500

    built a user card for an experiment

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,95 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home()
+  home: NinjaCard(),
 ));
+
+class NinjaCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('Some ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column (
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage('https://placekitten.com/200/300'),
+                radius: 40,
+              ),
+            ),
+            Divider(
+              height: 60, //not the thickness of the line
+              color: Colors.grey[800],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 1,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Some Name',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 1,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30),
+            Text(
+              'CURRENT LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 1,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Some Name2',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 1,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'somename@somthing.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18,
+                    letterSpacing: 1,
+                  ),
+                ),
+              ]
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
 class Home extends StatelessWidget {
   @override
